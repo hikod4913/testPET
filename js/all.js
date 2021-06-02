19f38d11a6ff68e697cb3e200bc0d62189ba6daf
hiko <hikod491314@icloud.com>
2021-06-02 14:20:30 +0800

    2021.06.02

--- js/all.js
+++ js/all.js
@@ -5,7 +5,7 @@ var swiper = new Swiper(".mySwiper", {
     loop: true,
     centeredSlides: true,
     autoplay: {
-        delay: 2500,
+        delay: 3000,
         disableOnInteraction: false,
     },
     pagination: {
@@ -25,3 +25,83 @@ swiper.el.onmouseover = function(){
 swiper.el.onmouseout = function(){
   swiper.autoplay.start();
 }
+// swiper
+
+
+// jQ
+
+$(function () {
+  let $window = $(window);
+  let $sub_nav = $('.sub-nav');
+  let win_height = $window.height();
+  let win_top = 0;
+  let back_top = $('.back-top');
+  let header = $('.header');
+  let header_height = header.height();
+  let menu_ham = $('.menu-ham');
+  let main_nav = $('.main-nav');
+
+  // moblie menu 
+  $window.on('resize', function () {
+    main_nav.removeClass('nav-move');
+    if ($window.width() < 992) {
+      $sub_nav.stop().slideUp();
+      // $sub_nav.addClass('sub-switch');
+    }
+    else {
+      $sub_nav.removeClass('sub-switch');
+      $sub_nav.stop().slideUp();
+      $sub_nav.stop().hover().slideDown();
+    }
+  })
+
+  
+  $('.list-title').click(function () {
+    $sub_nav.addClass('sub-switch');
+    $(this).next('.sub-switch').stop().slideToggle();
+    $(this).parent('li').siblings().children('.sub-switch').slideUp();
+  })
+
+  menu_ham.click(function () {
+    $sub_nav.stop().delay(600).slideUp();
+    $('body').toggleClass('body-fix');
+    main_nav.addClass('nav-move');
+  })
+
+  
+  // mobile menu
+
+  
+  $window.scroll(function () {
+    // back to top
+    win_top = $(this).scrollTop();
+    if (win_top >= win_height / 2) {
+       back_top.addClass('show-back');
+    }
+    else{
+       back_top.removeClass('show-back');
+    }
+
+    // fix
+    if (win_top > header_height) {
+        header.addClass('header-fix');
+        header.next().css({
+        'margin-top': header_height,
+      })
+    }
+    else {
+      header.removeClass('header-fix');
+      header.next().css({
+        'margin-top': 0
+      })
+    }
+  })
+
+  // click to top
+  back_top.click(function () {
+    $('html, body').stop().animate({scrollTop: 0});
+  })
+  
+
+  
+})
