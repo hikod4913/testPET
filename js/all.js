062a883611153dc5b6f11f3a716ce817581e1f34
hiko <hikod491314@icloud.com>
2021-06-07 19:50:57 +0800

    fix

--- js/all.js
+++ js/all.js
@@ -34,11 +34,50 @@ swiper.el.onmouseout = function(){
 // 行事曆
 document.addEventListener('DOMContentLoaded', function () {
   var calendarEl = document.getElementById('calendar');
+
   var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: 'dayGridMonth'
+    headerToolbar: {
+      left: 'title',
+      right: 'prev,next today'
+    },
+
+    displayEventTime: false, // don't show the time column in list view
+
+    // THIS KEY WON'T WORK IN PRODUCTION!!!
+    // To make your own Google API key, follow the directions here:
+    // http://fullcalendar.io/docs/google_calendar/
+    googleCalendarApiKey: 'AIzaSyAROK9ubm9f_L6VG-283sK72g6Z0ugX6gE',
+
+    // US Holidays
+    events: { googleCalendarId: 'petvogue2021@gmail.com' },
+    
+    eventClick: function(arg) {
+
+      // opens events in a popup window
+      window.open(arg.event.url, '_blank', 'width=600,height=400');
+
+      // prevents current tab from navigating
+      arg.jsEvent.preventDefault();
+    }
+
+
   });
   calendar.render();
-  calendar.setOption('height', 500);
+  if (window.screen.width < 992) {
+    calendar.setOption('height', 500);
+  }
+  else {
+    calendar.setOption('height', 600);
+  }
+  window.addEventListener('resize', function () {
+    if (window.screen.width < 992) {
+      calendar.setOption('height', 500);
+    }
+    else {
+      calendar.setOption('height', 600);
+    }
+  })
+  
 });
 // 行事曆
 
