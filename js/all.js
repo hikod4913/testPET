9ce8ed5cb368428d55d0a11273987731e45e27f7
hiko <hikod491314@icloud.com>
2021-06-09 05:14:37 +0800

    fix
    don't need window.addEvenListener('resize)

--- js/all.js
+++ js/all.js
@@ -111,94 +111,9 @@ document.addEventListener('DOMContentLoaded', function () {
   calendar.setOption('height', 'auto');
   calendar.setOption('contentHeight', 'auto');
 
-  window.addEventListener('resize', function () {
-    var calendarEl = document.getElementById('calendar-xs');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'timeGridFourDay',
-      headerToolbar: {
-        left: 'title',
-        right: 'prev,next'
-      },
-      views: {
-        timeGridFourDay: {
-          type: 'timeGrid',
-          duration: { days: 1 },
-          buttonText: '1 day'
-        }
-      },
-      slotMinTime : '09:00:00',
-      slotMaxTime : '18:00:00',
-      googleCalendarApiKey: 'AIzaSyAROK9ubm9f_L6VG-283sK72g6Z0ugX6gE',
-      events: { googleCalendarId: 'petvogue2021@gmail.com' },
-
-    });
-    calendar.render();
-    calendar.setOption('height', 'auto');
-    calendar.setOption('contentHeight', 'auto');
-
-    var calendarEl = document.getElementById('calendar-sm');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'timeGridFourDay',
-      headerToolbar: {
-        left: 'title',
-        right: 'prev,next'
-      },
-      views: {
-        timeGridFourDay: {
-          type: 'timeGrid',
-          duration: { days: 4 },
-          buttonText: '4 day'
-        }
-      },
-      slotMinTime : '09:00:00',
-      slotMaxTime : '18:00:00',
-      googleCalendarApiKey: 'AIzaSyAROK9ubm9f_L6VG-283sK72g6Z0ugX6gE',
-      events: { googleCalendarId: 'petvogue2021@gmail.com' },
-
-    });
-    calendar.render();
-    calendar.setOption('height', 'auto');
-    calendar.setOption('contentHeight', 'auto');
-
-    var calendarEl = document.getElementById('calendar-md');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridWeek',
-      headerToolbar: {
-        left: 'title',
-        right: 'prev,next'
-      },
-      googleCalendarApiKey: 'AIzaSyAROK9ubm9f_L6VG-283sK72g6Z0ugX6gE',
-      events: { googleCalendarId: 'petvogue2021@gmail.com' },
-
-    });
-    calendar.render();
-    calendar.setOption('height', 'auto');
-    calendar.setOption('contentHeight', 'auto');
-    
-    var calendarEl = document.getElementById('calendar-lg');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth',
-      headerToolbar: {
-        left: 'title',
-        right: 'prev,next today'
-      },
-      googleCalendarApiKey: 'AIzaSyAROK9ubm9f_L6VG-283sK72g6Z0ugX6gE',
-      events: { googleCalendarId: 'petvogue2021@gmail.com' },
-    });
-    calendar.render();
-    calendar.setOption('height', 'auto');
-    calendar.setOption('contentHeight', 'auto');
-  })
-
   
 });
 // 行事曆
-
-
-
-
-
-
 
 
 
