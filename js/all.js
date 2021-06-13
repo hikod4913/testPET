0de2d6bd345cece453473f9690d6838147aae7e5
hiko <hikod491314@icloud.com>
2021-06-13 21:14:53 +0800

    count fix

--- js/all.js
+++ js/all.js
@@ -7,26 +7,29 @@ let body_fix = document.querySelector('.body');
 let loading = document.querySelector('#loading');
 let progress = document.querySelector('#progress');
 let prg = 0;
+let about_info = document.querySelector('.about-info');
 
 window.onload = function () {
-    
     let time = setInterval(function () {
         body_fix.classList.add('body-fix');
         prg++;
-        progress.innerHTML = prg + '%';
-
         if (prg >= 100) {
+            prg = 100;
+            progress.innerHTML = prg + '%';
             window.scroll({
                 top: 0
             });
             loading.classList.add('loading-out');
             body_fix.classList.remove('body-fix');
-            
+            about_info.classList.remove('about-info-none');
             clearInterval(time);
 
             setTimeout(function(){
                 loading.remove();
             }, 3000)
+        }
+        else {
+            progress.innerHTML = prg + '%';
         }
     }, 10)
 }
