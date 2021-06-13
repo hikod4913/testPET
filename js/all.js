d69e030de7d3fa9685098419470fab67985e501c
hiko <hikod491314@icloud.com>
2021-06-14 02:07:50 +0800

    dom

--- js/all.js
+++ js/all.js
@@ -1,41 +1,33 @@
-// AOS
-AOS.init();
-// AOS
-
-let html_fix = document.querySelector('html');
 let body_fix = document.querySelector('.body');
 let loading = document.querySelector('#loading');
 let progress = document.querySelector('#progress');
-let prg = 0;
-let about_info = document.querySelector('.about-info');
 
-
+document.addEventListener('DOMContentLoaded', function () {
+    body_fix.classList.add('body-fix');
+    let imgs = document.querySelectorAll('img'),
+        show = 0,
+        num = 0;
+    let all = imgs.length;
+    [].slice.call(imgs).forEach(function (element, index) {
+        element.addEventListener('load', function () {
+            num++;
+            show = Math.floor(100 * num / all);
+            console.log(show);
+            progress.textContent = show + '%';
+        })
+        element.addEventListener('error', function () {
+            num++;
+            show = Math.floor(100 * num / all);
+            console.log(show);
+            progress.textContent = show + '%';
+        })
+    })
+})
 window.onload = function () {
-    let time = setInterval(function () {
-        body_fix.classList.add('body-fix');
-        prg++;
-        if (prg >= 100) {
-            prg = 100;
-            progress.innerHTML = prg + '%';
-            window.scroll({
-                top: 0
-            });
-            loading.classList.add('loading-out');
-            // about_info.classList.add('about-info-block');
-            clearInterval(time);
-
-            setTimeout(function(){
-                loading.remove();
-                window.scroll({
-                    top: 'auto'
-                });
-                body_fix.classList.remove('body-fix');
-                
-            }, 2000)
-        }
-        else {
-            progress.innerHTML = prg + '%';
-        }
-    }, 10)
+    loading.classList.add('loading-out');
+    setTimeout(function () {
+        body_fix.classList.remove('body-fix');
+        AOS.init();
+    }, 1000);
+    
 }
-
